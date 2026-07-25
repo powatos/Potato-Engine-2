1856b5984dc30257261885653f49a84967c8f19b
Sreejith <exetra63@gmail.com>
2026-07-24 23:59:08 -0400

    Asset configuration updates via CMake

--- CMake/MergeEngineProgramAssets.cmake
+++ CMake/MergeEngineProgramAssets.cmake
@@ -3,12 +3,12 @@ file(MAKE_DIRECTORY ${OUT_DIR})
 
 set(engine_files "")
 if (EXISTS ${ENGINE_DIR})
-	file(GLOB_RECURSE engine_files LIST_DIRECTORIES false RELATIVE ${ENGINE_DIR} ${ENGINE_DIR}/)
+	file(GLOB_RECURSE engine_files LIST_DIRECTORIES false RELATIVE ${ENGINE_DIR} ${ENGINE_DIR}/*)
 endif()
 
 set(game_files "")
 if (EXISTS ${GAME_DIR})
-	file(GLOB_RECURSE game_files LIST_DIRECTORIES false RELATIVE ${GAME_DIR} ${GAME_DIR}/)
+	file(GLOB_RECURSE game_files LIST_DIRECTORIES false RELATIVE ${GAME_DIR} ${GAME_DIR}/*)
 endif()
 
 foreach(item ${engine_files})
@@ -24,7 +24,7 @@ endforeach()
 foreach(item ${game_files})
 	get_filename_component(dest_dir ${OUT_DIR}/${item} DIRECTORY)
 	file(MAKE_DIRECTORY ${dest_dir})
-	file(COPY_FILE ${ENGINE_DIR}/${item} ${OUT_DIR}/${item} ONLY_IF_DIFFERENT)
+	file(COPY_FILE ${GAME_DIR}/${item} ${OUT_DIR}/${item} ONLY_IF_DIFFERENT)
 endforeach()
 
 message(STATUS "Program assets merge into engine assets complete")
